689106531c5f5dc0ee1bc6449b7f03a1d02a12ee
Bong <borobong230@gmail.com>
2023-12-05 22:49:03 +0900

    docs: 리드미 파일 수정 (도메인 추가)

--- fetchTest.js
+++ fetchTest.js
@@ -1,8 +1,7 @@
 import fetch from "node-fetch";
 
 const fetchTest = () => {
-  fetch(`http://3.37.227.153:3000/fortune/all`)
-    // fetch(`http://localhost:3000/fortune/average`)
+  fetch(`http://www.fortuneapi.app/fortune/all`)
     .then((response) => response.json())
     .then((data) => {
       console.log("Answer:", data.fortune);
---
+++ test.js
@@ -0,0 +1,7 @@
+const person = "bong";
+
+const personObj = {
+  person: "active",
+};
+
+console.log(personObj); //
